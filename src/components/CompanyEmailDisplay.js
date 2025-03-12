1dce2567b7e6f4c8ed83072c674a6b8c268adb63
David <daviddufau@hotmail.co.uk>
2025-03-12 16:36:37 +0100

    feat: Adds components for customer details

---
+++ src/components/CompanyEmailDisplay.js
@@ -0,0 +1,12 @@
+const CompanyEmailDisplay = ({ email }) => {
+    return (
+        <div className="company-email-container">
+            <div className="company-email-details">
+                <h3>{email} </h3>
+            </div>
+
+        </div>
+    )
+}
+
+export default CompanyEmailDisplay
---
+++ src/components/ContactDisplay.js
@@ -0,0 +1,12 @@
+const ContactDisplay = ({ contact }) => {
+    return (
+        <div className="contact-container">
+            <div className="contact-details">
+                <h3>{contact} </h3>
+            </div>
+
+        </div>
+    )
+}
+
+export default ContactDisplay
